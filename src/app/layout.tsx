7e2aa037e62d5c3c6d3423d0fd379f11d3ad61bf
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2025-12-17 08:41:06 +0500

    Update title in layout to 'Precision Data Strategies'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 // import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
-  title: "PDS",
+  title: "Precision Data Strategies",
   description: "AI Automation Company",
 };
 
